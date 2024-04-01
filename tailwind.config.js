78353eb984d6e2560d9892864ae7b36013f3a2f2
sibayarn <mysibayaneduardandreimaruquez@gmail.com>
2024-04-01 11:53:34 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        dark: "#2b2b2b",
+        light: "#ffffff",
+        berde: "#006341",
+        lberde: "#d4e9e2",
+      }
+    },
+  },
+  plugins: [],
+}
